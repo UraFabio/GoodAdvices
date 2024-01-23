a068a5fe2414093095c0c27625b8e196b817150f
FabioUra <fabioura10@gmail.com>
2024-01-23 10:40:12 -0300

    fisrt commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,74 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="main-container">
+    <NavBar />
+    <div id="advices-container">
+      <Advice />
+      <Advice />
+      <Advice />
+      <Advice />
+      <Advice />
+      <Advice />
+      <Advice />
+      <Advice />
+      <Advice />
+      <Advice />
+      <Advice />
+      <Advice />
+    </div>
+    <Footer />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import Advice from "./components/Advice.vue"
+import NavBar from "./components/NavBar.vue"
+import Footer from "./components/Footer.vue"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+   Advice,
+   NavBar,
+   Footer
   },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+*{
+  margin: 0;
+  padding: 0;
+  border: 0;
 }
+
+#main-container {
+  height: 100vh;
+  min-width: 1024px;
+  min-height: 768px;
+}
+
+#advices-container {
+  padding-top: 20px;
+  height: 100%;
+  min-height: 538px;
+  background-color: #ccc;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr; 
+  grid-auto-rows: 250px;
+  justify-items: center;
+  align-items: center;
+}
+
+a{
+  text-decoration: none;
+  color: #ccc;
+  margin: 12px;
+  transition: .5s;
+}
+
+a:hover {
+  color: #37a8f3;
+}
+
 </style>
---
+++ src/components/Advice.vue
@@ -0,0 +1,83 @@
+<template>
+    <div id="advice-container">
+        <div></div>
+        <p>{{ msg }}</p>
+        <button class="refresh-btn" @click="getAdvice">refresh</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Advice',
+    data() {
+        return {
+            msg:""
+        }
+    },
+    methods:{
+        async getAdvice(){
+
+            const numeroDecimal = Math.random();
+            const id = Math.floor(numeroDecimal * 224) + 1
+
+            const req = await fetch(`https://api.adviceslip.com/advice/${id}`)
+
+            const data = await req.json()
+            this.msg = data.slip.advice
+            //this.msg = data.
+        }
+    },
+    mounted() {
+        this.getAdvice()
+    }
+}
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Outfit:wght@400;500&family=Roboto:wght@100;300;500;700&display=swap');
+
+
+#advice-container {
+    margin: 20px;
+    padding: 10px;
+    background-color: rgb(164, 169, 231);
+    width: 200px;
+    height: 200px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-weight: 400;
+    font-family: Outfit, sans-serif;
+    border-radius: 100px;
+}
+
+#advice-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+}
+
+#advice-container p{
+    text-align: center;
+}
+
+
+.refresh-btn{
+    background-color: #323268;
+    color: #ccc;
+    font-weight: bold;
+    border: 1px solid #222;
+    border-radius: 100px;
+    padding: 5px;
+    font-size: 14px;
+    margin: 0 auto;
+    cursor: pointer;
+    transition: .5s;
+}
+
+
+.refresh-btn:hover{
+    background-color: transparent;
+    color: #222;
+}
+</style>
